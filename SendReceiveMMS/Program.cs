02c03dd9b5dcad995e642a9f204cc8f7dbfce80a
Mike Hamer <mhamer@bandwidth.com>
2020-10-29 16:45:53 -0400

    Add send and receive MMS implementation

---
+++ SendReceiveMMS/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Bandwidth.Standard;
+using Bandwidth.Standard.Http.Response;
+using Bandwidth.Standard.Messaging.Exceptions;
+using Bandwidth.Standard.Messaging.Models;
+
+namespace SendReceiveMMS
+{
+    class Program
+    {
+        // Bandwidth provided messaging token.
+        private static readonly string Token = System.Environment.GetEnvironmentVariable("BANDWIDTH_MESSAGING_TOKEN");
+        
+        // Bandwidth provided messaging secret.
+        private static readonly string Secret = System.Environment.GetEnvironmentVariable("BANDWIDTH_MESSAGING_SECRET");
+
+        // Bandwidth provided application id.
+        private static readonly string ApplicationId = System.Environment.GetEnvironmentVariable("BANDWIDTH_MESSAGING_APPLICATION_ID");
+
+        // Bandwidth provided account id.
+        private static readonly string AccountId = System.Environment.GetEnvironmentVariable("BANDWIDTH_ACCOUNT_ID");
+
+        // The phone number to send the message from.
+        private static readonly string From = System.Environment.GetEnvironmentVariable("BANDWIDTH_FROM");
+        
+        // The phone number to send the message to.
+        private static readonly string To = System.Environment.GetEnvironmentVariable("BANDWIDTH_TO");
+
+        // The media to send to the "to" phone number.
+        private static readonly string Media = "https://cdn2.thecatapi.com/images/MTY3ODIyMQ.jpg";
+
+        static async Task Main(string[] args)
+        {
+            // Creates a Bandwidth client instance for creating messages.
+            var client = new BandwidthClient.Builder()
+                .Environment(Bandwidth.Standard.Environment.Production)
+                .MessagingBasicAuthCredentials(Token, Secret)
+                .Build();
+
+            // A message request containing the required information to create a message using the client.
+            var request = new MessageRequest() {
+                ApplicationId = ApplicationId,
+                To = new List<string> { To },
+                From = From,
+                Media = new List<string> { Media }
+            };
+
+            // Creates and sends an MMS message with the provided message request.
+            try
+            {
+                var response = await client.Messaging.APIController.CreateMessageAsync(AccountId, request);
+                Console.WriteLine($"Create message response status code '{response.StatusCode}'.");
+            }
+            catch (MessagingException e)
+            {
+                var body = ((HttpStringResponse)e.HttpContext.Response).Body;
+                Console.WriteLine($"A messaging exception has occurred. {e.Message}");
+                Console.WriteLine(body);
+                System.Environment.Exit(-1);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"An unknown exception has occurred. {e.Message}");
+                System.Environment.Exit(-1);
+            }
+        }
+    }
+}
---
+++ Server/Controllers/CallbacksController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bandwidth.Standard.Messaging.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CallbacksController : ControllerBase
+    {
+        private readonly ILogger<CallbacksController> _logger;
+
+        public CallbacksController(ILogger<CallbacksController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost("messageCallback")]
+        public async Task<ActionResult> Messages()
+        {
+            _logger.LogInformation("Received message callback request.");
+
+            using var reader = new StreamReader(Request.Body, Encoding.UTF8);
+            var body = await reader.ReadToEndAsync();
+
+            var message = JsonConvert.DeserializeObject<IEnumerable<BandwidthCallbackMessage>>(body).First();
+
+            _logger.LogInformation($"{message.Type} {message.Description}");
+
+            switch (message.Type)
+            {
+                case "message-received":
+                    _logger.LogInformation(message.Message.Media.Any()
+                        ? $"Message received with '{message.Message.Media.Count}' media." : "Message received with no media.");
+                    break;
+                case "message-sending":
+                    _logger.LogInformation("Message is sending to the carrier.");
+                    break;
+                case "message-delivered":
+                    _logger.LogInformation("Message delivered from Bandwidth's network.");
+                    break;
+                case "message-failed":
+                    _logger.LogInformation("Messaged failed to be delivered.");
+                    break;
+                default:
+                    _logger.LogInformation("Unknown message type received.");
+                    break;
+            }
+
+            return new OkResult();
+        }
+    }
+}
